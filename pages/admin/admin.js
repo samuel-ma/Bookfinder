ea8033a95ed8a140953854ece2d59f414b4abd85
Samuel-ma <mabilmakur@gmail.com>
2024-04-15 01:28:41 +0200

    another commit

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -17,9 +17,21 @@ closeBtn.onclick = function () {
     modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// working with the dropdown
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
-    if (event.target == modal) {
+    if (!event.target.matches(".dropbtn") && event.target == modal) {
         modal.style.display = "none";
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
     }
 };
